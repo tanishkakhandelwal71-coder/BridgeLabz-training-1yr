c45151f3e8b7db54f5d831fbcc218d53d2cb61e4
tanishkakhandelwal71-coder~ <tanishikakhandelwal71@gmail.com>
2026-02-04 14:38:42 +0530

    array-method

---
+++ Practice programs/ArraysMethods.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+class ArraysMethods
+{
+public static void main (String args[])
+{
+int arr[] = {40, 10, 30, 20};
+System.out.println("original array");
+System.out.println("arr");
+
+System.out.println("Array.toString(arr)");
+Arrays.sort(arr);
+System.out.println("sorted array");
+}
+}
