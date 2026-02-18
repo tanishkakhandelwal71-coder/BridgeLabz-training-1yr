a744996df724c023d64c9682607bb87de1766542
tanishkakhandelwal71-coder~ <tanishikakhandelwal71@gmail.com>
2026-02-18 15:22:12 +0530

    Encapsulation

---
+++ Practice programs/Encapsulation.java
@@ -0,0 +1,10 @@
+public class Encapsulation {
+	public static void main(String[] args)
+	{
+		StudentData obj = new StudentData();
+		
+				obj.setMarks(90);
+				
+		System.out.println(obj.getMarks());
+	}
+}
---
+++ Practice programs/StudentData.java
@@ -0,0 +1,13 @@
+public class StudentData {
+	private int marks;
+	
+	
+	public void setMarks(int m) {
+		if(m>=0) {
+			marks = m;
+		}
+	} 
+	public int getMarks() {
+		return marks;
+	}
+}
