8377e3133eb1563714617dcca4f1911ccc18bb54
tanishkakhandelwal71-coder~ <tanishikakhandelwal71@gmail.com>
2026-04-09 15:04:45 +0530

    set

---
+++ Practice programs/SetPractice.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class SetPractice {
+	public static void main (String[]args) {
+HashSet<Integer> hs = new HashSet<>();
+hs.add(10);
+hs.add(23);
+hs.add(98);
+System.out.println("=======Hashset========");
+System.out.println(hs);
+LinkedHashSet<Integer> ls = new LinkedHashSet<>();
+TreeSet<Integer> ts = new TreeSet<>();
+}
+}
+
+
