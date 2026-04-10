1e61993ed76a09f5cfef4991e58eba4dcf8323f0
tanishkakhandelwal71-coder~ <tanishikakhandelwal71@gmail.com>
2026-04-10 11:46:22 +0530

    Map

--- Practice programs/SetPractice.java
+++ Practice programs/SetPractice.java
@@ -11,7 +11,23 @@ hs.add(98);
 System.out.println("=======Hashset========");
 System.out.println(hs);
 LinkedHashSet<Integer> ls = new LinkedHashSet<>();
+ls.add(45);
+ls.add(56);
+ls.add(58);
+ls.add(98);
+System.out.println("========LinkedSet=====");
+System.out.println(ls);
 TreeSet<Integer> ts = new TreeSet<>();
+ts.add(45);
+ts.add(67)
+ts.add(82);
+ts.add(48);
+System.out.println("========TreeSet=========");
+System.out.println(ts);
+
+
+
+
 }
 }
 
